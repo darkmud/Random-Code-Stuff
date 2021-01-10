9f500e1ec65ef1adf68fe4b8ee2af8e77d1d500c
darkmud <76975622+darkmud@users.noreply.github.com>
2021-01-10 03:07:56 -0500

    Update attendance.py

--- ANG Attendance Script/attendance.py
+++ ANG Attendance Script/attendance.py
@@ -1,9 +1,21 @@
 # I was too lazy to write down all the names of my team, so I made a script so I could input their 
 # names into the IDLE shell and it would count the attendance for me and show me the missing members.
-
 flag = 0
 runcount = 0
 
+def missing():
+    print("\n" + "Absentees:")
+    for value in dict:
+        if dict[value] == 'ABSENT':
+            print(value)
+    print("\n")
+
+def here():
+    print("\n" + "Present:")
+    for value in dict:
+        if dict[value] == 'PRESENT':
+            print(value)
+    print("\n")  
 
 dict = {'CHANDLER': 'ABSENT',
         'BERGHOFF': 'ABSENT',
@@ -22,26 +34,13 @@ while flag == 0:
     for key in dict:
         if lastname == key:
             runcount += 1
+            dict[key] = 'PRESENT'
 
     if lastname == 'missing':
-        print("\n" + "Absentees:")
-        for value in dict:
-            if dict[value] == 'ABSENT':
-                print(value)
-        print("\n")
-
-    if lastname == 'here':
-        print("\n" + "Present:")
-        for value in dict:
-            if dict[value] == 'PRESENT':
-                print(value)
-        print("\n")
-
-    for key in dict:
-        if lastname == key:
-            dict[key] = 'PRESENT'
+        missing()
+    elif lastname == 'here':
+        here()
 
     if runcount == len(dict):
         flag = 1
         print("ALL ACCOUNTED FOR")
-        
