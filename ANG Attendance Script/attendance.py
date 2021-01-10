cbc8f623d8ae45af62eb2168dc2edc095d4553dc
darkmud <76975622+darkmud@users.noreply.github.com>
2021-01-10 03:18:49 -0500

    Update attendance.py

--- ANG Attendance Script/attendance.py
+++ ANG Attendance Script/attendance.py
@@ -1,8 +1,21 @@
 # I was too lazy to write down all the names of my team, so I made a script so I could input their 
 # names into the IDLE shell and it would count the attendance for me and show me the missing members.
+
+# Variable and Dictionary declarations
 flag = 0
 runcount = 0
 
+dict = {'CHANDLER': 'ABSENT',
+        'BERGHOFF': 'ABSENT',
+        'BOWLES': 'ABSENT',
+        'COLE': 'ABSENT',
+        'JAMBAM': 'ABSENT',
+        'DANNER': 'ABSENT',
+        'DULL': 'ABSENT',
+        'FERGUSON': 'ABSENT',
+        'WEILAND': 'ABSENT'}
+
+# Function Declarations
 def missing():
     print("\n" + "Absentees:")
     for value in dict:
@@ -15,32 +28,25 @@ def here():
     for value in dict:
         if dict[value] == 'PRESENT':
             print(value)
-    print("\n")  
+    print("\n")
 
-dict = {'CHANDLER': 'ABSENT',
-        'BERGHOFF': 'ABSENT',
-        'BOWLES': 'ABSENT',
-        'COLE': 'ABSENT',
-        'JAMBAM': 'ABSENT',
-        'DANNER': 'ABSENT',
-        'DULL': 'ABSENT',
-        'FERGUSON': 'ABSENT',
-        'WEILAND': 'ABSENT'}
+# Main Function
+if __name__ == "__main__":
+    while flag == 0:
+        
+        lastname = input("Who's here bro: ")
 
-while flag == 0:
-    
-    lastname = input("Who's here bro: ")
+        for key in dict:
+            if lastname == key:
+                runcount += 1
+                dict[key] = 'PRESENT'
 
-    for key in dict:
-        if lastname == key:
-            runcount += 1
-            dict[key] = 'PRESENT'
+        if lastname == 'missing':
+            missing()
+        elif lastname == 'here':
+            here()
 
-    if lastname == 'missing':
-        missing()
-    elif lastname == 'here':
-        here()
-
-    if runcount == len(dict):
-        flag = 1
-        print("ALL ACCOUNTED FOR")
+        if runcount == len(dict):
+            flag = 1
+            print("ALL ACCOUNTED FOR")
+            
